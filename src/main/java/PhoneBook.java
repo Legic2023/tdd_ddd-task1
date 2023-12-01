492f4e6b5fc2ce503a818438d60e7bd5d9ee8ea0
Legic <Legic2007@yandex.ru>
2023-12-01 23:31:48 +0300

    Stub and tests for printAllNames

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -25,4 +25,8 @@ public class PhoneBook {
         return phoneBook.get(name);
     }
 
+    public static void printAllNames() {
+
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,6 +1,9 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PhoneBookTest {
@@ -41,5 +44,31 @@ public class PhoneBookTest {
         assertEquals(original, result);
 
     }
+
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    public void testPrintAllNames() {
+        // given:
+
+        // when:
+        PhoneBook.printAllNames();
+        // then:
+        assertEquals("TestName", outputStreamCaptor.toString());
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
 
 }
