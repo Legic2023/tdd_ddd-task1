bf55aeabc1915c8203ce690e1b2cd131761473de
Legic <Legic2007@yandex.ru>
2023-12-01 22:49:14 +0300

    findByNumber implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,14 +1,24 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class PhoneBook {
     static HashMap<String, Long> phoneBook = new HashMap<>();
+
+    public void phoneBook() {
+        PhoneBook.phoneBook.put("TestName", 9999999999L);
+    }
+
     public static int add(String name, long number) {
         phoneBook.put(name, number);
         return phoneBook.size();
     }
 
     public static String findByNumber(long number) {
-        return null;
+        Optional<String> result = phoneBook.entrySet().stream()
+                .filter(n -> n.getValue().equals(number))
+                .map(Map.Entry::getKey)
+                .findFirst();
+
+        return result.orElse("");
     }
 
 }
